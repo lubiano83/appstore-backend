6250e4682dc517f2a48c484f93ee136d378672c3
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-08-16 15:28:24 -0400

    agregando modulo aplicaxciones

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,22 +13,27 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll( @Query('email') email?: string ) {
+    return this.usersService.findAll(email);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get(':nombre')
+  findOne(@Param('nombre') nombre: string) {
+    return this.usersService.findOne(nombre);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Put(':nombre')
+  update(@Param('nombre') nombre: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(nombre, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete(':nombre')
+  remove(@Param('nombre') nombre: string) {
+    return this.usersService.remove(nombre);
   }
+
+  @Post(':nombre/app/:id')
+  downloadApp(@Param('nombre') nombre: string, @Param('id') id: string) {
+    return this.usersService.downloadApp(nombre, +id);
+  };
 }
